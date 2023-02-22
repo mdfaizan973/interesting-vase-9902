1ef7d474af47a21a457c8bf9d9b711d5fde96916
mdfaizan973 <faizan.md9735@gmail.com>
2023-02-22 17:54:27 +0530

    added  cakes card

--- choco-flowers/src/Pages/Carousel.jsx
+++ choco-flowers/src/Pages/Carousel.jsx
@@ -5,7 +5,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ExclusiveGifts from "../Cards/ExclusiveGifts";
-
+import Cakes from "../Cards/Cakes";
 export default function Carousel() {
   var settings = {
     dots: true,
@@ -67,6 +67,7 @@ export default function Carousel() {
         </Slider>
         <hr />
         <ExclusiveGifts />
+        <Cakes />
       </Box>
     </>
   );
