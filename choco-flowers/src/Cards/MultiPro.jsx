71a5cb8fb331119fb65bf6b6454d354efd955a4d
mdfaizan973 <faizan.md9735@gmail.com>
2023-02-23 10:34:40 +0530

    added some effect

--- choco-flowers/src/Cards/MultiPro.jsx
+++ choco-flowers/src/Cards/MultiPro.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SimpleGrid, Box, Image, Heading } from "@chakra-ui/react";
+import { SimpleGrid, Box, Image, Heading, Link } from "@chakra-ui/react";
 export default function MultiPro() {
   return (
     <>
@@ -11,69 +11,140 @@ export default function MultiPro() {
         gap={4}
         columns={[2, 3, 4]}
       >
-        <Box>
-          <Image
-            borderRadius="full"
-            boxSize="250px"
-            src="https://www.fnp.com/assets/images/custom/new-desk-home/cakes/2022/Butterscotch.jpg"
-            alt="Dan Abramov"
-          />
+        <Box w="85%">
+          <Box borderRadius="full" overflow="hidden">
+            <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+              <Image
+                transform="scale(1.0)"
+                src="https://www.fnp.com/assets/images/custom/new-desk-home/cakes/2022/Butterscotch.jpg"
+                alt="some text"
+                objectFit="contain"
+                width="100%"
+                transition="0.4s ease-in-out"
+                _hover={{
+                  transform: "scale(1.15)",
+                }}
+              />
+            </Link>
+          </Box>
         </Box>
-        <Box>
-          <Image
-            borderRadius="full"
-            boxSize="250px"
-            src="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-feelings-of-love-gift-hamper-110722-m.jpg"
-            alt="Dan Abramov"
-          />
+        <Box w="85%">
+          <Box borderRadius="full" overflow="hidden">
+            <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+              <Image
+                transform="scale(1.0)"
+                src="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-feelings-of-love-gift-hamper-110722-m.jpg"
+                alt="some text"
+                objectFit="contain"
+                width="100%"
+                transition="0.3s ease-in-out"
+                _hover={{
+                  transform: "scale(1.05)",
+                }}
+              />
+            </Link>
+          </Box>
         </Box>
-        <Box>
-          <Image
-            borderRadius="full"
-            boxSize="250px"
-            src="https://www.fnp.com/assets/images/custom/new-desk-home/cakes/2022/Flowers-N-Cakes.jpg"
-            alt="Dan Abramov"
-          />
+        <Box w="85%">
+          <Box borderRadius="full" overflow="hidden">
+            <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+              <Image
+                transform="scale(1.0)"
+                src="https://www.fnp.com/assets/images/custom/new-desk-home/cakes/2022/Flowers-N-Cakes.jpg"
+                alt="some text"
+                objectFit="contain"
+                transition="0.4s ease-in-out"
+                _hover={{
+                  transform: "scale(1.15)",
+                }}
+              />
+            </Link>
+          </Box>
         </Box>
-        <Box>
-          <Image
-            borderRadius="full"
-            boxSize="250px"
-            src="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-dream-a-little-98548-m.jpg"
-            alt="Dan Abramov"
-          />
+        <Box w="85%">
+          <Box borderRadius="full" overflow="hidden">
+            <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+              <Image
+                transform="scale(1.0)"
+                src="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-dream-a-little-98548-m.jpg"
+                alt="some text"
+                objectFit="contain"
+                width="100%"
+                transition="0.3s ease-in-out"
+                _hover={{
+                  transform: "scale(1.05)",
+                }}
+              />
+            </Link>
+          </Box>
         </Box>
-        <Box>
-          <Image
-            borderRadius="full"
-            boxSize="250px"
-            src="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-luxe-floral-fountain-in-vase-135737-m.jpg"
-            alt="Dan Abramov"
-          />
+        <Box w="85%">
+          <Box borderRadius="full" overflow="hidden">
+            <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+              <Image
+                transform="scale(1.0)"
+                src="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-luxe-floral-fountain-in-vase-135737-m.jpg"
+                alt="some text"
+                objectFit="contain"
+                width="100%"
+                transition="0.4s ease-in-out"
+                _hover={{
+                  transform: "scale(1.15)",
+                }}
+              />
+            </Link>
+          </Box>
         </Box>
-        <Box>
-          <Image
-            borderRadius="full"
-            boxSize="250px"
-            src="https://cdn4.fruitbouquets.com/wcsstore/Flowers/images/catalog/192558mx.jpg?height=456&width=418&sharpen=a0.5,r1,t1&quality=80&auto=webp&optimize={medium}"
-            alt="Dan Abramov"
-          />
+        <Box w="85%">
+          <Box borderRadius="full" overflow="hidden">
+            <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+              <Image
+                transform="scale(1.0)"
+                src="https://cdn4.fruitbouquets.com/wcsstore/Flowers/images/catalog/192558mx.jpg?height=456&width=418&sharpen=a0.5,r1,t1&quality=80&auto=webp&optimize={medium}"
+                alt="some text"
+                objectFit="contain"
+                width="100%"
+                transition="0.4s ease-in-out"
+                _hover={{
+                  transform: "scale(1.15)",
+                }}
+              />
+            </Link>
+          </Box>
         </Box>
-        <Box>
-          <Image
-            borderRadius="full"
-            boxSize="250px"
-            src="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-the-delilah-187572-m.jpg"
-            alt="Dan Abramov"
-          />
+        <Box w="85%">
+          <Box borderRadius="full" overflow="hidden">
+            <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+              <Image
+                transform="scale(1.0)"
+                src="https://res.cloudinary.com/interflora/f_auto,q_auto,t_pnopt7prodlp/products/p-the-delilah-187572-m.jpg"
+                alt="some text"
+                objectFit="contain"
+                width="100%"
+                transition="0.4s ease-in-out"
+                _hover={{
+                  transform: "scale(1.15)",
+                }}
+              />
+            </Link>
+          </Box>
         </Box>
-        <Box>
-          <Image
-            borderRadius="full"
-            boxSize="250px"
-            src="https://cdn4.fruitbouquets.com/wcsstore/Flowers/images/catalog/193099mbx.jpg?height=456&width=418&sharpen=a0.5,r1,t1&quality=80&auto=webp&optimize={medium}"
-            alt="Dan Abramov"
-          />
+        <Box w="85%">
+          <Box borderRadius="full" overflow="hidden">
+            <Link textDecoration="none" _hover={{ textDecoration: "none" }}>
+              <Image
+                transform="scale(1.0)"
+                src="https://www.fnp.com/assets/images/custom/new-desk-home/cakes/2022/Pineapple.jpg"
+                alt="some text"
+                objectFit="contain"
+                width="100%"
+                transition="0.4s ease-in-out"
+                _hover={{
+                  transform: "scale(1.15)",
+                }}
+              />
+            </Link>
+          </Box>
         </Box>
       </SimpleGrid>
     </>
